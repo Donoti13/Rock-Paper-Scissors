c90377a300f0c87d7749386a6b1e5624da6bf2ea
Leon Saracini <141871578+Donoti13@users.noreply.github.com>
2024-01-14 14:31:29 +0100

    Create function getCOmputerChoice() Randomply return "Rock", "Paper" or "Scissor"

---
+++ rockpaperscissors.js
@@ -0,0 +1,30 @@
+
+// START WITH A FUNCTION THAT WILL RANDOMELY RETURN EITHER:
+// ROCK, PAPER OF SCISSORS.
+
+
+let randomValue;
+
+function getComputerChoice() {
+
+    randomValue = Math.floor(Math.random() * 3);
+
+
+  if (randomValue === 2) {
+
+    console.log("rock");
+  } 
+
+   else if (randomValue === 1) {
+
+    console.log("paper");
+   }
+
+    else {
+
+        console.log("scissors");
+    }
+
+}
+    getComputerChoice();
+
